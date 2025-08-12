d4b4817b58dc550efad731d443f7ca950b003211
abdulrhmanmagdy1 <abdulrhmanmagdy13@gmail.com>
2025-08-11 23:53:45 -0700

    i2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,14 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'recipe/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const response = await fetch('https://your-api-endpoint.com/recipes');
+      const recipes = await response.json();
+      return recipes.map((recipe: { id: string }) => ({ id: recipe.id }));
+    }
   }
 ];
